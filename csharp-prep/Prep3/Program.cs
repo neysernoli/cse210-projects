b944d0c057e2e8255d742bba29edf9f064ad20b6
Neyser <neysernoli@gmail.com>
2024-09-21 22:39:45 -0500

    Resolved Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        int numberg = 0;
+
+        while (numberg != number)
+        {
+            Console.Write("What is your guess? ");
+            numberg = int.Parse(Console.ReadLine());
+            
+            if (numberg > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (numberg < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
     }
 }
